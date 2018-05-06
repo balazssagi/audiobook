d7e13b743e2c8cf864a45430975d3b6f4db10a95
Balázs Sági <balazssagi@outlook.com>
2018-05-06 14:25:37 +0200

    fix

--- src/lib/Auth.js
+++ src/lib/Auth.js
@@ -1,11 +1,15 @@
-
+const env = process.env.NODE_ENV
 const CLIENT_ID =
-  '01b8666f08ab1a5cbcbefebd3006e555ce7f9f71d8b20b1cd956ceea8434818d'
-const REDIRECT_URL = 'https://localhost:3000/'
+  env === 'production'
+    ? '142334acb47277ab513043ac4bd3137a19924c18463f60da512d7239deb27f7e'
+    : '01b8666f08ab1a5cbcbefebd3006e555ce7f9f71d8b20b1cd956ceea8434818d'
+const REDIRECT_URL =
+  env === 'production'
+    ? 'https://musing-edison-10a77b.netlify.com/'
+    : 'https://localhost:3000/'
 const STORAGE_KEY = 'token'
 
 export default class Auth {
-
   static oAuthUrl = `https://be.contentful.com/oauth/authorize?response_type=token&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=content_management_manage`
 
   static saveAccessToken() {
